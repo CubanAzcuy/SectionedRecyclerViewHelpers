ad0242740bcfd24c664a8bf0573e17ac177be09b
Robert Gross <mrgross17@gmail.com>
2018-08-28 17:27:20 -0400

    Change Modifier to be open

--- sectionrecyclerviewhelper/src/main/java/com/dev925/sectionrecyclerviewhelper/section/UpdatableSection.kt
+++ sectionrecyclerviewhelper/src/main/java/com/dev925/sectionrecyclerviewhelper/section/UpdatableSection.kt
@@ -1,10 +1,10 @@
 package com.dev925.sectionrecyclerviewhelper.section
 
-import com.dev925.sectionrecyclerviewhelper.utils.ChangeRequest
 import com.dev925.sectionrecyclerviewhelper.listeners.UpdatableListener
+import com.dev925.sectionrecyclerviewhelper.utils.ChangeRequest
 import java.util.*
 
-class UpdatableSection: Section() {
+open class UpdatableSection: Section() {
     internal var updateSectionObservable: UpdatableListener<List<ChangeRequest>>? = null
 
     protected fun updateSection(changeRequests: ArrayList<ChangeRequest>) {
